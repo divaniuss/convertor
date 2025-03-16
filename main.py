dfc1a8f6c9a2a53d4628e5ce343b6c204cc3f2c6
Dmitriy <dimasikemelka@gmail.com>
2025-03-16 18:06:59 +0200

    first commit for time

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 class Converter:
-    def init(self):
-        self.units = {
+    def __init__(self):
+        self.distances = {
             1: 1000,
             2: 100,
             3: 1,
             4: 0.001
         }
+        self.time = {
+            1: 1,
+            2: 60,
+            3: 3600,
+            4: 86400
+        }
 
-    def convert(self):
+    def convert_distance(self):
         print("1. Миллиметр\n"
               "2. Сантиметр\n"
               "3. Метр\n"
@@ -17,14 +23,44 @@ class Converter:
         choice_into = int(input("Введите вашу конечную единицу измерения (1-4): "))
         value = float(input("Введите значение для конвертации: "))
 
-        if choice_from not in self.units or choice_into not in self.units:
+        if choice_from not in self.distances or choice_into not in self.distances:
             print("Ошибка: некорректный ввод единиц измерения.")
             return
 
+        result = value / self.distances[choice_from] * self.distances[choice_into]
 
-        result = value / self.units[choice_from] * self.units[choice_into]
+        print(f"Результат: {result}")
+
+    def convert_time(self):
+        print("1. Секунда\n"
+              "2. Минута\n"
+              "3. Час\n"
+              "4. День\n")
+
+        choice_from = int(input("Введите вашу начальную единицу измерения (1-4): "))
+        choice_into = int(input("Введите вашу конечную единицу измерения (1-4): "))
+        value = float(input("Введите значение для конвертации: "))
+
+        if choice_from not in self.time or choice_into not in self.time:
+            print("Ошибка: некорректный ввод единиц измерения.")
+            return
+
+        result = value / self.time[choice_from] * self.time[choice_into]
 
         print(f"Результат: {result}")
 
 c = Converter()
-c.convert()
+
+
+while True:
+    choice = str(input("Вы хотите конвертировать дистанции(1) или время(2): "))
+
+    if choice == "1":
+        c.convert_distance()
+        break
+    elif choice == "2":
+        c.convert_time()
+        break
+    else:
+        print("Введите правильные значения")
+
